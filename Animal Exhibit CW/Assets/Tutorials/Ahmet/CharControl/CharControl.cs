6bc19cc709749c9acf27d3bc6bf45c11c4970acf
Ahmet-Sevinc <73647777+Ahmet-Sevinc@users.noreply.github.com>
2020-10-30 14:40:04 +0000

    Finished Lego Character control

---
+++ Animal Exhibit CW/Assets/Tutorials/Ahmet/CharControl/CharControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+
+public class CharControl : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float maxSpeed = 100.0f; //F = float value
+    public float acc = 5.0f;
+    public float dec = 5.0f;
+    public float rotation = 0.0f;
+
+    void Start()
+    {
+       
+    }
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            if (speed < maxSpeed)  //ACCELERATE
+                speed += acc;
+        }
+        else
+        {
+            if (speed > 0)        //DECELERATE
+                speed -= dec;
+        }
+        transform.position += transform.forward * Time.deltaTime * speed;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -0.5f, 0);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0.5f, 0);
+        }
+    }
+}
