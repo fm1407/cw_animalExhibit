85a6c70c77929676d599bb91b27d569c031c204c
fm1407 <filiz.misirlioglu.147@gmail.com>
2020-10-30 21:57:02 +0000

    filiz tut 4 part 1 and 2

---
+++ Animal Exhibit CW/Assets/Tutorials/Filiz/Ui Btn Filiz/Tutorial 04 01/ExploderSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExploderSlider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator anim;
+    public Slider slider;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.Play("Explode", -1, slider.value);
+    }
+}
