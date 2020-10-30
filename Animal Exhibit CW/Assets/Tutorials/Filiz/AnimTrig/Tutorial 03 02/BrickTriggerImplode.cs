6f65b6842b27fc5f8208d0e3dde24af007a47b17
fm1407 <filiz.misirlioglu.147@gmail.com>
2020-10-30 20:26:40 +0000

    filiz tutorial3 part 1 and 2
    Did my tutorial 3 pt 1 and 2

---
+++ Animal Exhibit CW/Assets/Tutorials/Filiz/AnimTrig/Tutorial 03 02/BrickTriggerImplode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickTriggerImplode : MonoBehaviour
+{
+    public GameObject brickArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnMouseDown()
+    {
+        brickArray.GetComponent<Animator>().Play("Implode");
+    }
+}
