fe9600ad5fd1babaf4e8001d1259ea986dd3d922
fm1407 <filiz.misirlioglu.147@gmail.com>
2020-11-03 13:31:26 +0000

    soreya script files

---
+++ Animal Exhibit CW/Assets/Tutorials/Soreya/Ui Btn Soreya/Button/AnimPlaySoreya.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimPlaySoreya: MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+
+            anim.Play("Walk", -1, 0f);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            anim.Play("Flip", -1, 0f);
+        }
+
+    }
+}
---
+++ Animal Exhibit CW/Assets/Tutorials/Soreya/Ui Btn Soreya/Slider/RotatorSliderSoreya.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotatorSliderSoreya : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider slider;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, slider.value, 0);
+    }
+}
