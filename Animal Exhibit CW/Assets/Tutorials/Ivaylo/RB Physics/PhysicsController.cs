8aae7f8070303d56ec60722c617533e6f6cbcaa5
ivohristov23 <hristovfifa@abv.bg>
2020-11-03 12:59:49 +0200

    ivo update
    update

---
+++ Animal Exhibit CW/Assets/Tutorials/Ivaylo/RB Physics/PhysicsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsController : MonoBehaviour
+{
+	//PSEUDO CODE: Declare private variable of type RigidBody, public float speed;
+	
+	private Rigidbody rb;
+	public float speed;
+	// Start is called before the first frame update
+	void Start()
+	{
+		// Initialize RigidBody variable by getting from current gameobject;
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Fixed Update is called once per frame - use fixed Update for physics
+	void FixedUpdate()
+	{
+
+		// Check for "a", "d", "w", and "d" key input, for each, add force vector3s going left, right, forward, and back
+		if (Input.GetKey("a"))
+		{
+			rb.AddForce(Vector3.left);
+		}
+		if (Input.GetKey("d"))
+		{
+			rb.AddForce(Vector3.right);
+		}
+		if (Input.GetKey("w"))
+		{
+			rb.AddForce(Vector3.forward);
+		}
+		if (Input.GetKey("s"))
+		{
+            rb.AddForce(Vector3.back);
+			//Stretch Task - add multiple to increase force
+		}
+	}
+}
+
+	//Stretch Tasks
+	// Unity Rollaball (Combines forces in Vector https://learn.unity.com/project/roll-a-ball-tutorial)
+	// Intermediate controllers https://medium.com/ironequal/unity-character-controller-vs-rigidbody-a1e243591483
---
+++ Animal Exhibit CW/Assets/Tutorials/Ivaylo/Ui Btn Ivo/ExploderSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExploderSlider : MonoBehaviour
+{
+    Animator anim;
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.Play("Explode", -1, slider.value);
+    }
+}
