2bb98befc87f29aab20695b52c0cf80dd337001f
Ahmet-Sevinc <73647777+Ahmet-Sevinc@users.noreply.github.com>
2020-12-10 12:59:42 +0000

    added camera movement, following cursor

--- Animal Exhibit CW/Assets/MAIN/Scenes/camera.cs
+++ Animal Exhibit CW/Assets/MAIN/Scenes/camera.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class camera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
+    
 }
