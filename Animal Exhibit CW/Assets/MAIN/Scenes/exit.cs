7a0d5a8eeff64aa8207dc1d5b845cc4637fedcc2
Ahmet-Sevinc <73647777+Ahmet-Sevinc@users.noreply.github.com>
2020-12-12 15:50:10 +0000

    exit message

---
+++ Animal Exhibit CW/Assets/MAIN/Scenes/exit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class exit : MonoBehaviour
+{
+    public GameObject Exit_panel;
+    public void OpenExitPanel()
+    {
+        if (Exit_panel != null)
+        {
+            Exit_panel.SetActive(true);
+        }
+    }
+    void Start()
+    {
+
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
---
+++ Animal Exhibit CW/Assets/MAIN/Scenes/quitApplication.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class quitApplication : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Application is closing");
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
