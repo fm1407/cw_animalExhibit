915278ff53231c6cf8d36f85bb6e442e7ee0d127
Ahmet-Sevinc <73647777+Ahmet-Sevinc@users.noreply.github.com>
2020-12-13 19:13:33 +0000

    added cameras

---
+++ Animal Exhibit CW/Assets/MAIN/Scenes/CameraSwitch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Camera IntroCam;
+    public Camera TpsCam;
+    bool introCam = true;
+    void Start()
+    {
+        IntroCam.enabled = introCam;
+        TpsCam.enabled = !introCam;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            introCam = !introCam;
+            IntroCam.enabled = introCam;
+            TpsCam.enabled = !introCam;
+        }
+    }
+}
