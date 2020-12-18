503a13375b08df72a329e01de673738338994f37
fm1407 <filiz.misirlioglu.147@gmail.com>
2020-12-18 21:21:20 +0000

    f8

---
+++ Animal Exhibit CW/Assets/MAIN/Scenes/filiz_script/to DINO/mainToTrex.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainToTrex : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        SceneManager.LoadScene("trex");
+    }
+}
+
+
