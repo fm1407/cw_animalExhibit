33f2178c56a2863e20e24ba550393ac21c8c2a10
ayerose <soreya-k@hotmail.de>
2020-12-25 20:09:41 +0000

    lego animation

---
+++ Animal Exhibit CW/Assets/MAIN/Scenes/soreya_script/LegoMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LegoMovement : MonoBehaviour
+{
+
+    GameObject gameController;
+    //  NavMeshAgent myNavMeshAgent;
+
+    void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController");
+        //     myNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+
+        GetComponent<NavMeshAgent>().destination = gameController.transform.position;
+
+    }
+}
